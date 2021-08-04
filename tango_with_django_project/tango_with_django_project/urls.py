4f4d175131a89a5305d8006dde97cd9ff9be68dd
Shiping Yong <2598902y@student.gla.ac.uk>
2021-08-04 13:09:19 +0100

    change url

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     path('accounts/register/', MyRegistrationView.as_view(), name='registration_register'),
     path('oauth/', include('social_django.urls', namespace='social')), 
     path('accounts/', include('registration.backends.simple.urls')),
-    path('accounts/login', include('allauth.urls')),
+    path('googlelog/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
